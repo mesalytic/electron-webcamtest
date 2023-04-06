1b27c8a54ac56dbe4d8c85c8d797894ca1d806d2
chocololat <lgurvan81@gmail.com>
2023-04-06 12:33:45 +0200

    work day push

---
+++ src/emailPage.js
@@ -0,0 +1,14 @@
+/**
+ * TODO:
+ * Get checkboxes, get email, and add database input
+ * TODO:
+ * Also redirect
+ * */
+
+document.addEventListener('DOMContentLoaded', () => {
+    var button = document.getElementById("test");
+
+    button.addEventListener("click", () => {
+        alert("see ya later");
+    })
+})
--- src/indexPage.js
+++ src/indexPage.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    var button = document.getElementById("test");
+    var button = document.getElementById("begin");
     
     button.addEventListener("click", () => {
-        document.location.href = "./webcam.html"
+        document.location.href = "./email.html"
     })
 })
